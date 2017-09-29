c3eb68f0a485dc9a191dc575d1059c99e8f5d31f
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2017-09-29 12:09:34 +0700

    data transfer from front and back-function

---
+++ routes/index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+const fileUpload = require('express-fileupload');
+var PDF = require('pdfkit');
+var fs = require('fs');
+
+router.use(fileUpload());
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var path = req.query.path;
+  console.log('path = '+path);
+  res.render('index.ejs', {path: path});
+});
+
+router.post('/test', function(req, res, next) {
+  console.log(req.body);
+
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+  console.log('files found.');
+  console.log(req.files);
+  res.status(200).send('Okay');
+  // console.log('/test api call');
+});
+
+router.post('/upload', function(req, res, next) {
+  // if (!req.files)
+  //   return res.status(400).send('No files were uploaded.');
+  //
+  console.log("POST CALL");
+  let fileUploaded = req;
+  console.log(fileUploaded);
+
+  // writePDF(fileUploaded, './seed/pdf/testing_file.pdf', res);
+
+  // res.send('done');
+  // fileUploaded.mv('./seed/img/img.jpg', function(err) {
+  //   if (err)
+  //     return res.status(500).send(err);
+  //
+  //   res.send('File uploaded!');
+  // });
+
+});
+
+function writePDF(fileUploaded, path, res) {
+  let doc = new PDF();
+  let writeStream = fs.createWriteStream(path, {flags: "wx"});
+
+  writeStream.on("open", () => {
+    console.log("Writing to " + encodeURIComponent(path));
+    res.send({helo: path}); //DEBUG
+  });
+  writeStream.on("error", (err) => {
+      if (err.code == "EEXIST") {
+          console.log(path + " already exists")
+          writeStream.end();
+
+          path = "."+path.split(".")[1]+"-new.pdf"
+          writePDF(fileUploaded, path, res);
+      } else {
+          // Re-raise the error
+          throw err;
+      }
+  });
+
+  doc.pipe(writeStream);
+  doc.image(fileUploaded.data, 0, 0, {scale: 0.5});
+  doc.end();
+
+}
+module.exports = router;
